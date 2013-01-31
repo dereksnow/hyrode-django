1b85865fcd35f7f50a52cf7ecb28d35f9f606ea9
Derek Snow <derek.snow@gmail.com>
2013-01-31 11:41:27 -0600

    Created Link Model and added bookmarks to INSTALLED_APPS

--- skillfare/skillfare/settings.py
+++ skillfare/skillfare/settings.py
@@ -133,6 +133,7 @@ THIRD_PARTY_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
+    'bookmarks',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
