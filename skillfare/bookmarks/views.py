edc3c50e4b15c1983e96feb03aa7d686a4b49e1a
Derek Snow <derek.snow@gmail.com>
2013-02-01 10:40:56 -0600

    modified main_page view to use template  and added user_page view

--- skillfare/bookmarks/views.py
+++ skillfare/bookmarks/views.py
@@ -1,16 +1,27 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import Context
+from django.template.loader import get_template
+from django.contrib.auth.models import User
 
 def main_page(request):
-	output = u'''
-		<html>
-			<head><title>%s</title></head>
-			<body>
-				<h1>%s</h1><p>%s</p>
-			</body>
-		</html>
-			''' % (u'Skillfare', 
-				u'Skillfare',
-			 u'Find, Share & Organize your Learning Resources')
+	template = get_template('main_page.html')
+	variables = Context({'head_title': u'SkillFare', 
+		'page_title': u'SkillFare', 
+		'page_body': u'Find, Share & Organize your Learning Resources'})
+	output = template.render(variables)
+	return HttpResponse(output)
+
+def user_page(request, username):
+	try:
+		user = User.objects.get(username=username)
+	except User.DoesNotExist:
+		raise Http404(u'Requested user not found.')
+
+	bookmarks = user.bookmark_set.all()
+
+	template = get_template('user_page.html')
+	variables = Context({'username': username, 'bookmarks': bookmarks})
+	output = template.render(variables)
 	return HttpResponse(output)
 
