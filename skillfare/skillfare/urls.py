c6de46add2ff0c07bb5a85988a56f195b00ec4cf
Derek Snow <derek.snow@gmail.com>
2013-02-03 17:06:29 -0600

    added urfconfs for subclassing RegistrationForm and PasswordChangeForm

--- skillfare/skillfare/urls.py
+++ skillfare/skillfare/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from bookmarks.views import *
+from django.contrib.auth import views as auth_views
+from password_validation.forms import ValidPasswordRegistrationForm, ValidPasswordChangeForm
+from registration.views import register
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,7 +11,16 @@ from bookmarks.views import *
 urlpatterns = patterns('',
 
 	url(r'^$', main_page),
-	url(r'^user/(\w+)/$', user_page),
+	url(r'^user/([\w.@+-]+)/$', user_page),
+    url(r'^accounts/register/$', register,
+		{'backend': 'registration.backends.default.DefaultBackend', 
+	 	'form_class': ValidPasswordRegistrationForm},
+		name='registration_register'),
+
+	url(r'^accounts/password/change/$', auth_views.password_change, 
+		{'password_change_form': ValidPasswordChangeForm},
+		name='auth_password_change'),    
+
 	url(r'^accounts/', include('registration.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
