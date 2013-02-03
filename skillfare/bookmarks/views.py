d8e6ca34b2ea3c456144db375ba1ed3f58282a6e
Derek Snow <derek.snow@gmail.com>
2013-02-03 16:58:01 -0600

    changed views to use render instead of render_to_request

--- skillfare/bookmarks/views.py
+++ skillfare/bookmarks/views.py
@@ -1,12 +1,11 @@
 # Create your views here.
 from django.http import Http404
-from django.template import Context
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.contrib.auth.models import User
 
 def main_page(request):
-	variables = Context({'user': request.user})
-	return render_to_response('main_page.html', variables)
+	variables = {'user': request.user}
+	return render(request, 'main_page.html', variables)
 
 def user_page(request, username):
 	try:
@@ -16,6 +15,6 @@ def user_page(request, username):
 
 	bookmarks = user.bookmark_set.all()
 
-	variables = Context({'username': username, 'bookmarks': bookmarks})
-	return render_to_response('user_page.html', variables)
+	variables = {'username': username, 'bookmarks': bookmarks}
+	return render(request, 'user_page.html', variables)
 
