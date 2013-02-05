3dd9e8eb19d9b728a997bfb8a37e7ab0d535952c
Derek Snow <derek.snow@gmail.com>
2013-02-04 19:35:39 -0600

    removed Tag class after adding django-tagging app

--- skillfare/bookmarks/models.py
+++ skillfare/bookmarks/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+import tagging
 
 class Link(models.Model):
 	url = models.URLField(unique=True)
+
+	def __unicode__(self):
+		return self.url
 
 class Bookmark(models.Model):
 	title = models.CharField(max_length=200)
 	user = models.ForeignKey(User)
 	link = models.ForeignKey(Link)	
+
+	def __unicode__(self):
+		return u' %s, %s, %s' % (self.title, self.user.username, 
+			self.link.url)
+
+# Register Models with django-tagging
+tagging.register(Bookmark)
+
+
 
 
