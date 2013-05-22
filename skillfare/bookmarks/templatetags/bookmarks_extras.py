69c0c8b6f0b9e2f4a923dd777d4ccdbe87eb1ed7
Derek Snow <derek.snow@gmail.com>
2013-05-22 11:56:58 -0500

    added custom filter to map feature to feature icon

---
+++ skillfare/bookmarks/templatetags/bookmarks_extras.py
@@ -0,0 +1,7 @@
+from django import template
+register = template.Library()
+
+@register.filter(name='font_icon')
+def get_font_icon(key):
+    bookmark_features = {'Certificates':'icon-trophy', 'Feedback':'icon-exchange', 'Sandbox':'icon-terminal', 'Video':'icon-film'}
+    return bookmark_features.get(key)
