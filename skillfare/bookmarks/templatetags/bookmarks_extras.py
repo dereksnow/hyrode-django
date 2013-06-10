c6d7263377da672fc80c0469b0ef328abe3d6a05
Derek Snow <derek.snow@gmail.com>
2013-06-10 17:05:49 -0500

    changed sandbox icon to flask (beaker according to font awesome) from terminal prompt to be more generic

--- skillfare/bookmarks/templatetags/bookmarks_extras.py
+++ skillfare/bookmarks/templatetags/bookmarks_extras.py
@@ -3,5 +3,5 @@ register = template.Library()
 
 @register.filter(name='font_icon')
 def get_font_icon(key):
-    bookmark_features = {'Certificates':'icon-trophy', 'Feedback':'icon-exchange', 'Sandbox':'icon-terminal', 'Video':'icon-film'}
+    bookmark_features = {'Certificates':'icon-trophy', 'Feedback':'icon-exchange', 'Sandbox':'icon-beaker', 'Video':'icon-film'}
     return bookmark_features.get(key)
